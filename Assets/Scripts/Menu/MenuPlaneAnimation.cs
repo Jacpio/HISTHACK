63d218db8b81264f11461aa3ab55652a320cca69
SzlamaDev <milosz.szlama@szlamadev.pl>
2023-04-14 10:27:12 +0200

    Menu Commit

--- Assets/Scripts/Menu/MenuPlaneAnimation.cs
+++ Assets/Scripts/Menu/MenuPlaneAnimation.cs
@@ -3,16 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MenuPlaneAnimation : MonoBehaviour
-{
-  
-    void Start()
     {
-        
+    public Vector2 endPos;
+    private Vector2 startPos;
+    private float x,y;
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private void Start()
+    {
+        startPos = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(endPos.x > 0) { 
+            if (gameObject.transform.position.x < endPos.x)
+            {
+                x = Mathf.Lerp(endPos.x, gameObject.transform.position.x, Time.fixedDeltaTime * moveSpeed);
+                y = Mathf.Lerp(endPos.y, gameObject.transform.position.y, Time.fixedDeltaTime * moveSpeed);
+                rb.velocity = new Vector2(x, y);
+            }
+            else
+            {
+                gameObject.transform.position = startPos;
+
+            }
+        }
+        else
+        {
+            if (gameObject.transform.position.x > endPos.x)
+            {
+                x = Mathf.Lerp(endPos.x, gameObject.transform.position.x, Time.fixedDeltaTime * moveSpeed);
+                y = Mathf.Lerp(endPos.y, gameObject.transform.position.y, Time.fixedDeltaTime * moveSpeed);
+                rb.velocity = new Vector2(x, y);
+            }
+            else
+            {
+                gameObject.transform.position = startPos;
+
+            }
+        }
     }
 }
